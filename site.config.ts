56bc8293d2254317ad63cf55d4e8d41244aaac58
yyq83 <82815012+yyq83@users.noreply.github.com>
2023-03-15 19:27:09 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,20 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
-
+  rootNotionPageId: '9ae922767ab546d19d48fd2c9b645bd7',//notion页面id
+  //https://yangyinqi.notion.site/9ae922767ab546d19d48fd2c9b645bd7
+  //https://www.notion.so/yangyinqi/4996a757cf2e4185b7b9526301302cde?pvs=4
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '方法评估',
+  domain: 'https://yangyinqi.notion.site/9ae922767ab546d19d48fd2c9b645bd7',
+  author: 'yangyinqi',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'method description',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
